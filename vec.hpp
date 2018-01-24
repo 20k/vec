6659c56cf4417cf45b98c58172f594a6b29e34ef
James <Icedshot@gmail.com>
2018-01-24 03:51:34 +0000

    fix approx equal function

--- vec.hpp
+++ vec.hpp
@@ -669,7 +669,7 @@ vec<N, T> sqrtf(const vec<N, T>& v)
 }
 
 template<typename T, typename U>
-approx_equal(T v1, U v2, float bound = 0.0001f)
+bool approx_equal(T v1, U v2, float bound = 0.0001f)
 {
     return v1 >= v2 - bound && v1 < v2 + bound;
 }
